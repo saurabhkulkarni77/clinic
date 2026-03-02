a736161453a083dd66b32c899e0df5fffc59b56c
Saurabh Kulkarni <sskkulk@gmail.com>
2026-03-01 22:35:27 -0500

    Update app.py

--- app.py
+++ app.py
@@ -39,7 +39,7 @@ if st.session_state["authentication_status"]:
     
     # Configure AI
     genai.configure(api_key=api_key)
-    model = genai.GenerativeModel('gemini-1.5-flash')
+    model = genai.GenerativeModel('gemini-2.5-flash')
 
     st.title("🩺 Pain Severity & Anatomy Agent")
     
